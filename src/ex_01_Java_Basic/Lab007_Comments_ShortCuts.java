3850713968ee796bdcea59da977e24cf53768168
AmolTarapure16 <amoltarapure16@gmail.com>
2025-06-09 14:54:19 +0530

    Java Comments Keywords Identifiers

---
+++ src/ex_01_Java_Basic/Lab007_Comments_ShortCuts.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basic;
+
+public class Lab007_Comments_ShortCuts
+{
+    // This is SingleLine Comments
+
+    /* This is
+    Multiline
+    Comments */
+
+    public static void main(String[] args) {
+        System.out.println("This is SingleLine Comments shotcuts ->  ctrl+/ ");
+        System.out.println("This is MultiLine Comments shotcuts -> ctrl+shift+/");
+    }
+
+}
