582cb4152a14157d1d12b117afad9309e9296a7d
AmolTarapure16 <amoltarapure16@gmail.com>
2025-06-10 15:45:55 +0530

    ex_02_Variables_DataType & ex_03_Literals

---
+++ src/ex_02_Variables_DataTypes/Lab020_Interview_QnA2.java
@@ -0,0 +1,13 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab020_Interview_QnA2
+{
+    public static void main(String[] args) {
+        //        int enum = 10;
+        int Enum = 10;
+        int a_b = 89;
+//        int amol tarapure  = 89;
+        int amol$tatrapure = 90;
+        int Lab013 = 123;
+    }
+}
---
+++ src/ex_02_Variables_DataTypes/Lab022_Numerics_Data_Types.java
@@ -0,0 +1,15 @@
+package ex_02_Variables_DataTypes;
+
+public class Lab022_Numerics_Data_Types
+{
+    public static void main(String[] args) {
+        byte b = 100;
+        // b = 128;
+        // b = -127;
+        int i = 10;
+        short s = 90;
+        char c= 'a';
+        long l = 987654321012l;
+        long l1 = 9876543210L;
+    }
+}
