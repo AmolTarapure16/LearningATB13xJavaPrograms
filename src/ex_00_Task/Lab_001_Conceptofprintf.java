b7b123ac877d47b3907fac1032f1cdc2b03cce8a
AmolTarapure16 <amoltarapure16@gmail.com>
2025-06-10 16:07:35 +0530

    Task - concept of printf() and print the table of 9

---
+++ src/ex_00_Task/Lab_001_Conceptofprintf.java
@@ -0,0 +1,49 @@
+package ex_00_Task;
+
+public class Lab_001_Conceptofprintf
+{
+    public static void main(String[] args)
+    {
+        int a = 9;
+        int b = 1;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+
+    }
+}
