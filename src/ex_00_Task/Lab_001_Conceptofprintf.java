c1d390e87ca41b2075cbc9bd344d91abc17b1c30
AmolTarapure16 <amoltarapure16@gmail.com>
2025-06-11 09:41:18 +0530

    Task1 - concept of printf() and print the table of 9

--- src/ex_00_Task/Lab_001_Conceptofprintf.java
+++ src/ex_00_Task/Lab_001_Conceptofprintf.java
@@ -4,46 +4,16 @@ public class Lab_001_Conceptofprintf
 {
     public static void main(String[] args)
     {
-        int a = 9;
-        int b = 1;
-
-        System.out.printf("%d x %d = %d",a,b,a*b);
-        System.out.println();
-        b++;
-
-        System.out.printf("%d x %d = %d",a,b,a*b);
-        System.out.println();
-        b++;
-
-        System.out.printf("%d x %d = %d",a,b,a*b);
-        System.out.println();
-        b++;
-
-        System.out.printf("%d x %d = %d",a,b,a*b);
-        System.out.println();
-        b++;
-
-        System.out.printf("%d x %d = %d",a,b,a*b);
-        System.out.println();
-        b++;
-
-        System.out.printf("%d x %d = %d",a,b,a*b);
-        System.out.println();
-        b++;
-
-        System.out.printf("%d x %d = %d",a,b,a*b);
-        System.out.println();
-        b++;
-
-        System.out.printf("%d x %d = %d",a,b,a*b);
-        System.out.println();
-        b++;
-
-        System.out.printf("%d x %d = %d",a,b,a*b);
-        System.out.println();
-        b++;
-
-        System.out.printf("%d x %d = %d",a,b,a*b);
-
+        int num = 9;
+        System.out.printf("%d x %d = %d",num,1,num).println();
+        System.out.printf("%d x %d = %d",num,2,num*2).println();
+        System.out.printf("%d x %d = %d",num,3,num*3).println();
+        System.out.printf("%d x %d = %d",num,4,num*4).println();
+        System.out.printf("%d x %d = %d",num,5,num*5).println();
+        System.out.printf("%d x %d = %d",num,6,num*6).println();
+        System.out.printf("%d x %d = %d",num,7,num*7).println();
+        System.out.printf("%d x %d = %d",num,8,num*8).println();
+        System.out.printf("%d x %d = %d",num,9,num*9).println();
+        System.out.printf("%d x %d = %d",num,10,num*10);
     }
 }
