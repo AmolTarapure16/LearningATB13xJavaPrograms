9e8a1b5e72b2a3f30f6de27e883217e2369f4ec6
AmolTarapure16 <amoltarapure16@gmail.com>
2025-06-17 10:12:46 +0530

    17 - June ex_10_For_Loop and ex_11_While

---
+++ src/ex_10_For_Loop/Lab099_For_LOOP_VAR_NAME.java
@@ -0,0 +1,14 @@
+package ex_10_For_Loop;
+
+public class Lab099_For_LOOP_VAR_NAME
+{
+    public static void main(String[] args) {
+
+        for (int amol = 0; amol < 10; amol++) {
+            System.out.println(amol);
+
+        }
+
+
+    }
+}
---
+++ src/ex_10_For_Loop/Lab103_For_If_LOOP.java
@@ -0,0 +1,19 @@
+package ex_10_For_Loop;
+
+public class Lab103_For_If_LOOP
+{
+    public static void main(String[] args)
+    {
+        for(int amol=0;amol<18;amol++)
+        {
+            if(amol>15)
+            {
+                System.out.println("Gift From Papa : iphone");
+            }
+            else
+            {
+                System.out.println("he will get only hugs! or Candy!");
+            }
+        }
+    }
+}
