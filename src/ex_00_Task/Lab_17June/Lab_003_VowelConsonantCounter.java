2a1cf1fd2838f4acb42944d024694f6ce8d390f6
AmolTarapure16 <amoltarapure16@gmail.com>
2025-06-18 09:47:50 +0530

    17 - June Task

--- src/ex_00_Task/Lab_17June/Lab_003_VowelConsonantCounter.java
+++ src/ex_00_Task/Lab_17June/Lab_003_VowelConsonantCounter.java
@@ -1,33 +1,25 @@
 package ex_00_Task.Lab_17June;
 
+import java.util.Scanner;
+
 public class Lab_003_VowelConsonantCounter
 {
     public static void main(String[] args)
     {
-    String input = "Hello World!";
-    int vowelCount = 0;
-    int consonantCount = 0;
-
-    input = input.toLowerCase();
-
-    for(int i=0;i<input.length();i++)
-    {
-       char ch= input.charAt(i);
-
-       if(Character.isLetter(ch))
-       {
-           if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-           {
-               vowelCount++;
-           }
-           else
-           {
-               consonantCount++;
-           }
-       }
-        System.out.println("Vowels: " + vowelCount);
-        System.out.println("Consonants: " + consonantCount);
-    }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the single char");
+        char ch = scanner.next().toLowerCase().charAt(0);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch=='o' || ch=='u')
+        {
+            System.out.println(ch +" is vowel!");
+        }
+        else if(ch >='a' && ch<='z')
+        {
+            System.out.println(ch +" is consonent");
+        }
+        else{
+            System.out.println("Invalid Char!");
+        }
 
     }
 }
