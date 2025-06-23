7fcbdeb5348a0cd017a5c72f0f59ec07c433e300
AmolTarapure16 <amoltarapure16@gmail.com>
2025-06-23 11:00:34 +0530

    23 - June Task - (Basic If-Else)

---
+++ src/ex_00_Task/Lab_23June/Lab_006_EligibleToVote.java
@@ -0,0 +1,19 @@
+package ex_00_Task.Lab_23June;
+
+import java.util.Scanner;
+
+public class Lab_006_EligibleToVote
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Age");
+        int age = scanner.nextInt();
+
+        if (age>=18)
+        {
+            System.out.println("Eligible To Vote");
+        }else {
+            System.out.println("Not Eligible To Vote");
+        }
+    }
+}
---
+++ src/ex_00_Task/Lab_23June/Lab_008_LargestOfThreeNumbers.java
@@ -0,0 +1,31 @@
+package ex_00_Task.Lab_23June;
+
+import java.util.Scanner;
+
+public class Lab_008_LargestOfThreeNumbers
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter First num ");
+        int a = scanner.nextInt();
+        System.out.println("Enter Second num");
+        int b = scanner.nextInt();
+        System.out.println("Enter Third num");
+        int c = scanner.nextInt();
+
+        if(a>b)
+        {
+            System.out.println(a + " is Largest");
+        }
+        else if (b>c)
+        {
+            System.out.println(b + " is Largest");
+        }else
+        {
+            System.out.println(c + " is Largest");
+        }
+
+
+    }
+}
