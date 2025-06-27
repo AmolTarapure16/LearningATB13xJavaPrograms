0f0acbc3ddddcadc0bd8ac3c8b64c628e1ee6004
AmolTarapure16 <amoltarapure16@gmail.com>
2025-06-27 09:52:57 +0530

    27 - June Task - Done

---
+++ src/ex_00_Task/Lab_27June/Lab000_DiagonalSum.java
@@ -0,0 +1,26 @@
+package ex_00_Task.Lab_27June;
+
+public class Lab000_DiagonalSum
+{
+    public static void main(String[] args)
+    {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int n = matrix.length;
+        int primarySum = 0;
+        int secondarySum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            primarySum += matrix[i][i]; // Primary diagonal: i == j
+            secondarySum += matrix[i][n - 1 - i]; // Secondary diagonal: i + j == n - 1
+        }
+
+        System.out.println("Primary Diagonal Sum: " + primarySum);
+        System.out.println("Secondary Diagonal Sum: " + secondarySum);
+    }
+}
