cc3939d8a37da3b6758d2c53fbab391aa7ae7f26
AmolTarapure16 <amoltarapure16@gmail.com>
2025-07-01 14:51:55 +0530

    1 - July ex_17_OOPs ex_18_OOPs_Constructor

---
+++ src/ex_17_OOPs/Lab168_Cats.java
@@ -0,0 +1,13 @@
+package ex_17_OOPs;
+
+public class Lab168_Cats
+{
+    Lab168_Cats()
+    {
+        System.out.println("DC - Main Class");
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
