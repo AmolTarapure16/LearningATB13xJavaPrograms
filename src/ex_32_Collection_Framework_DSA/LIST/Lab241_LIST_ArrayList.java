adf9cedc416da0331714514911bf89e57ac55dfb
AmolTarapure16 <amoltarapure16@gmail.com>
2025-07-11 09:48:00 +0530

    11 - July - ex_32_Collection_Framework_DSA.LIST

---
+++ src/ex_32_Collection_Framework_DSA/LIST/Lab241_LIST_ArrayList.java
@@ -0,0 +1,17 @@
+package ex_32_Collection_Framework_DSA.LIST;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab241_LIST_ArrayList
+{
+    public static void main(String[] args) {
+
+     //   List l = new ArrayList(); // Dynamic Diapatch
+        ArrayList l = new ArrayList();
+        l.add("hello");
+        l.add(123);
+        l.add(true);
+        l.add("Blah Blah");
+    }
+}
